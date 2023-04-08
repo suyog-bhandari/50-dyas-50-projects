72084bdacc8a6d685d837057941b16830918da98
suyog-bhandari <108009756+suyog-bhandari@users.noreply.github.com>
2023-04-08 20:14:15 +0545

    Add files via upload

---
+++ day 9/script.js
@@ -0,0 +1,24 @@
+const sounds =['audio1', 'audio2', 'audio3', 'audio4', 'audio5']
+
+sounds.forEach(sound =>{
+    const btn =document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', ()=>{
+        stopSongs()
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs(){
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
